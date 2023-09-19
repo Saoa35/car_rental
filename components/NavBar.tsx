b0fd8e9b835b5addfe9ee82f992fbe62606e1ba2
Saoa35 <saoamah@gmail.com>
2023-09-19 09:13:21 +0400

    + added logo

--- components/NavBar.tsx
+++ components/NavBar.tsx
@@ -1,8 +1,24 @@
 // "use client";
 
 // import { CustomButtonProps } from "@/types";
+import Image from "next/image";
+import Link from "next/link";
 import React from "react";
 
 export const NavBar = () => {
-  return <div>NavBar</div>;
+  return (
+    <header className="w-full absolute z-10">
+      <nav className="max-w-[1440px] mx-auto flex justify-between items-center sm:px-16 px-6 py-4">
+        <Link href="/" className="flex justify-center items-center">
+          <Image
+            src="/logo.svg"
+            alt="Logo"
+            className="object-contain"
+            width={118}
+            height={18}
+          />
+        </Link>
+      </nav>
+    </header>
+  );
 };
