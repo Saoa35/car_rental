0178a3533fed384f4a51cdfac8572c68a3f4c7c8
Saoa35 <saoamah@gmail.com>
2023-09-19 20:09:32 +0400

    + created SearchManufacturer

---
+++ components/SearchManufacturer.tsx
@@ -0,0 +1,9 @@
+// "use client";
+
+import Image from "next/image";
+import Link from "next/link";
+import React from "react";
+
+export const SearchManufacturer = () => {
+  return <div></div>;
+};
