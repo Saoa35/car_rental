a96a497b11680438ef2b586b5c1230db4a39f9d2
Saoa35 <saoamah@gmail.com>
2023-09-19 20:20:55 +0400

    + created SearchManufacturerProps interface

--- components/SearchBar.tsx
+++ components/SearchBar.tsx
@@ -1,17 +1,25 @@
 "use client";
 
-import Image from "next/image";
-import Link from "next/link";
-import React from "react";
+// import Image from "next/image";
+// import Link from "next/link";
+import React, { useState } from "react";
+import { SearchManufacturer } from "./SearchManufacturer";
 
 export const SearchBar = () => {
+  const [manufacturer, setManufacturer] = useState("");
+
   const handleSearch = (e) => {
     e.preventDefault();
   };
 
   return (
     <form className="searchbar" onSubmit={handleSearch}>
-      <div className="searchbar__item"></div>
+      <div className="searchbar__item">
+        <SearchManufacturer
+          manufacturer={manufacturer}
+          setManufacturer={setManufacturer}
+        />
+      </div>
     </form>
   );
 };
--- components/SearchManufacturer.tsx
+++ components/SearchManufacturer.tsx
@@ -1,9 +1,9 @@
 // "use client";
 
-import Image from "next/image";
-import Link from "next/link";
+// import Image from "next/image";
+// import Link from "next/link";
 import React from "react";
 
-export const SearchManufacturer = () => {
+export const SearchManufacturer = ({ manufacturer, setManufacturer }) => {
   return <div></div>;
 };
