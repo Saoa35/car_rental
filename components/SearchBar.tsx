c15edaba069a1c203623bcc7830d6ac0903a25b6
Saoa35 <saoamah@gmail.com>
2023-09-22 20:26:03 +0400

    + added functionality to handleSearch func.

--- components/SearchBar.tsx
+++ components/SearchBar.tsx
@@ -2,15 +2,30 @@
 
 import React, { useState } from "react";
 import { SearchManufacturer } from "./SearchManufacturer";
+import Image from "next/image";
 
-const SearchButton = ({ otherClasses }) => (
-  <button type="submit" className={`-ml-3 z-10 ${otherClasses}`}></button>
+const SearchButton = ({ otherClasses }: { otherClasses: string }) => (
+  <button type="submit" className={`-ml-3 z-10 ${otherClasses}`}>
+    <Image
+      src="/magnifying-glass.svg"
+      alt="magnifying glass"
+      width={40}
+      height={40}
+      className="object-contain"
+    />
+  </button>
 );
 
 export const SearchBar = () => {
   const [manufacturer, setManufacturer] = useState("");
+  const [model, setModel] = useState("");
 
-  const handleSearch = () => {};
+  const handleSearch = (e: React.FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+    if (manufacturer === "" && model === "") {
+      return alert("Please fill in the search bar");
+    }
+  };
 
   return (
     <form className="searchbar" onSubmit={handleSearch}>
@@ -19,7 +34,29 @@ export const SearchBar = () => {
           manufacturer={manufacturer}
           setManufacturer={setManufacturer}
         />
+
+        <SearchButton otherClasses="sm:hidden" />
       </div>
+
+      <div className="searchbar__item">
+        <Image
+          src="/model-icon.png"
+          width={25}
+          height={25}
+          className="ml-4 absolute w-[20px] h-[20px]"
+          alt="model icon"
+        />
+        <input
+          type="text"
+          name="model"
+          value={model}
+          onChange={(e) => setModel(e.target.value)}
+          placeholder="Tiguan"
+          className="searchbar__input"
+        />
+        <SearchButton otherClasses="sm:hidden" />
+      </div>
+      <SearchButton otherClasses="max-sm:hidden" />
     </form>
   );
 };
