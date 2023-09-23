435947c950abd60f88ffa6813e8b498ef007d13e
Saoa35 <saoamah@gmail.com>
2023-09-23 08:34:02 +0400

    * edited fetchCars func.

--- app/page.tsx
+++ app/page.tsx
@@ -7,7 +7,7 @@ import { fetchCars } from "@/utils";
 export default async function Home({ searchParams }) {
   const allCars = await fetchCars({
     manufacturer: searchParams.manufacturer || "",
-    year: searchParams.year || 2022,
+    year: searchParams.year || 2023,
     fuel: searchParams.fuel || "",
     limit: searchParams.limit || 10,
     model: searchParams.model || "",
--- utils/index.ts
+++ utils/index.ts
@@ -1,13 +1,15 @@
 import { CarProps, FilterProps } from "@/types";
 
 export const fetchCars = async (filters: FilterProps) => {
+  const { manufacturer, year, model, limit, fuel } = filters;
+
   const headers = {
     "X-RapidAPI-Key": "f09583baa7msh25b04d5a1b69517p1801c5jsn81318439a7c0",
     "X-RapidAPI-Host": "cars-by-api-ninjas.p.rapidapi.com",
   };
 
   const response = await fetch(
-    "https://cars-by-api-ninjas.p.rapidapi.com/v1/cars?model=q3",
+    `https://cars-by-api-ninjas.p.rapidapi.com/v1/cars?make=${manufacturer}&year=${year}&model=${model}&limit=${limit}&fuel_type=${fuel}`,
     { headers: headers }
   );
 
